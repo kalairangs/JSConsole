9d41d1de694c6b701b66f70cfa3631242ff0c138
kalairangs <kalairangs@gmail.com>
2021-02-02 19:38:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
+import { fromEvent } from "rxjs";
+import { filter } from "rxjs/operators";
+import Interpreter from "./interpreter";
+import Visitor from "./visitors";
+const acorn = require("acorn");
+const App = () => {
+  let [inputValue, setInputValue] = React.useState()
+  let [prevData, setPrevData] = React.useState([]);
+  let [inputValueHis, setInputValueHis] = React.useState([]);
+  let recentValues = [];
+  let pos = useRef(0);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const inputRef = useRef(null);
+  let Value = inputRef.current;
+  const updateInputValue = () => {
+    setInputValue(inputRef.current.value);
+  };
+
+  React.useEffect(() => {
+    const Observable$ = fromEvent(Value, "keyup").pipe(
+      filter((e) => e.keyCode === 13 && !e.shiftKey)
+    );
+
+    if (Value !== null || undefined) {
+      Observable$.subscribe((e) => {
+        e.preventDefault();
+      });
+
+      const getPreviousData = (element, arrow_func) =>
+        fromEvent(element, "keyup").pipe(filter((e) => e.key === arrow_func));
+
+      let arrowUp = getPreviousData(Value, "ArrowUp");
+      let arrowDown = getPreviousData(
+        Value,
+        "ArrowDown"
+      );
+
+      arrowUp.subscribe(() => {
+        if (recentValues.length > 0 && pos.current >= 0) {
+          setInputValue(
+            recentValues[pos.current] ? recentValues[pos.current] : ""
+          );
+          pos.current = pos.current - 1;
+        }
+      });
+
+      arrowDown.subscribe(() => {
+        if (recentValues.length > 0 && pos.current < recentValues.length) {
+          setInputValue(
+            recentValues[pos.current] ? recentValues[pos.current] : ""
+          );
+          pos.current = pos.current + 1;
+        }
+      });
+    }
+  });
+
+  const interpretJsCode = (event) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      const value = inputRef.current.value.trim();
+      let eValue = "";
+      if (value) {
+        if (!/(var|let|const)/.test(value)) {
+          eValue = `print(${value})`;
+        }
+        try {
+          const body = acorn.parse(eValue || value, { ecmaVersion: 2020 }).body;
+          const jsInterpreter = new Interpreter(new Visitor());
+          jsInterpreter.interpret(body);
+          const answer = jsInterpreter.getValue();
+          const finalResult = answer ? value + "    =  " + answer : value;
+          setPrevData((prevHistory) => [...prevHistory, finalResult]);
+          setInputValueHis((prevValue) => [...prevValue, value]);
+          recentValues.push(...inputValueHis, value);
+          pos.current = recentValues.length; // intermiediatary
+          setInputValue("");
+        } catch {}
+      }
+    }
+  };
+
+  const showPrevData = (item, index) => (
+    <div key={index} style={{ height: "45px", textAlign: "left" }}>
+      <span style={{ color: "red" }}>{"> "}</span>
+      {item}
     </div>
   );
-}
+
+  return (
+    <div className="App">
+      <div>{prevData.length > 0 && prevData.map(showPrevData)}</div>
+      <div className="textarea">
+        <textarea
+          ref={inputRef}
+          value={inputValue}
+          onChange={updateInputValue}
+          onKeyDown={interpretJsCode}
+          style={{ width: "100%", height: "100px" }}
+          autoFocus
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/interpreter.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Interpreter extends React.Component {
+	constructor(visitor) {
+		super();
+		this.visitor = visitor;
+	}
+	interpret(nodes) {
+		return this.visitor.run(nodes);
+	}
+	getValue() {
+		return this.visitor.value;
+	}
+}
+export default Interpreter;
